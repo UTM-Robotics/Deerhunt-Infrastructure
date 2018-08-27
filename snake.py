90768b5971f212b264f93b606340fde1c83df8e9
Josh Wolfe <joshuafwolfe@gmail.com>
2018-08-27 19:20:49 -0400

    Added basic collision logic

--- snake.py
+++ snake.py
@@ -21,12 +21,32 @@ class Snake(Game):
             print('Food collision')
             exit(1)
         elif one and not two:
-            self.player_one.score += 1
+            self.player_one.size += 1
         elif two and not one:
-            self.player_two.score += 1
+            self.player_two.size += 1
 
         self.current_food = self.generate_new_food()
 
+    def does_collide(self, player_one, player_two):
+        return (player_one.x, player_one.y) in player_two.segments[:-1]
+
+    def handle_collision_checks(self):
+        # TODO: They can hit each other head on
+        if does_collide(player_one, player_one):
+            print('Player 1 self died')
+            exit(1)
+        elif does_collide(player_two, player_two):
+            print('Player 2 self died')
+            exit(1)
+        elif does_collide(player_one, player_two):
+            print('Player 1 hit player 2')
+            exit(1)
+        elif does_collide(player_two, player_one):
+            print('Player 2 hit player 1')
+            exit(1)
+
     def tick(self):
+        self.handle_food_checks()
+        self.handle_collision_checks()
         self.player_one.tick()
         self.player_two.tick()
