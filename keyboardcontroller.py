57c182ffd44874dc8c9c324a3123e63e6cdc7f53
Elijah Mirecki <elimirks@gmail.com>
2018-08-28 13:07:05 -0400

    Clean up keyboard controller

--- keyboardcontroller.py
+++ keyboardcontroller.py
@@ -5,21 +5,19 @@ class KeyboardController:
     def __init__(self, key_handler,
                left_key, right_key, up_key, down_key):
         self.key_handler = key_handler
-        self.left_key    = left_key
-        self.right_key   = right_key
-        self.up_key      = up_key
-        self.down_key    = down_key
-        self.direction   = Direction.DOWN
+        self.direction_map = {
+            left_key:  Direction.LEFT,
+            right_key: Direction.RIGHT,
+            up_key:    Direction.UP,
+            down_key:  Direction.DOWN,
+        }
+        # Initial direction
+        self.direction = Direction.DOWN
 
     def _get_new_direction(self):
-        if self.key_handler[self.right_key]:
-            return Direction.RIGHT
-        elif self.key_handler[self.left_key]:
-            return Direction.LEFT
-        elif self.key_handler[self.up_key]:
-            return Direction.UP
-        elif self.key_handler[self.down_key]:
-            return Direction.DOWN
+        for key,direction in self.direction_map.items():
+            if self.key_handler[key]:
+                return direction
         return self.direction
 
     def tick(self, player):
