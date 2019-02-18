2f6d578dfe5a83c4937fd0583ee37a503dbcd164
Josh Wolfe <joshuafwolfe@gmail.com>
2019-02-18 13:28:49 -0500

    Added helper functions

--- game/client/helper_classes.py
+++ game/client/helper_classes.py
@@ -1,4 +1,5 @@
 from move import Move
+from math import sqrt
 
 class Map:
     def __init__(self, map_grid):
@@ -7,6 +8,31 @@ class Map:
     def get_tile(self, x, y):
         return self.grid[y][x]
 
+    def is_wall(self, x, y):
+        return self.grid[y][x].lower() == 'x'
+
+    def is_resource(self, x, y):
+        return self.grid[y][x].lower() == 'r'
+
+    def closest_resources(self, unit):
+        locations = []
+        x, y = unit.position()
+        for row in range(len(self.grid)):
+            for col in range(len(self.grid[row])):
+                if self.is_resource(self, col, row):
+                    locations.append((row, col))
+
+        result = None
+        so_far = 999999
+        for (r, c) in locations:
+            dy = r-y
+            dx = c-x
+            dist = sqrt(dx**2 + dy**2)
+            if dist < so_far:
+                result = (r, c)
+                so_far = dist
+
+        return result
 
 class Units:
     def __init__(self, unit_dict):
@@ -37,3 +63,4 @@ class Unit:
 
     def mine(self):
         return (str(self.id), ['MINE'])
+
