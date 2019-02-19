7fdda9064565c2d566325a1be0dd86bf2baaa7cb
Josh Wolfe <joshuafwolfe@gmail.com>
2019-02-19 16:45:50 -0500

    Added logic for docker submissions

---
+++ website/src/components/Submit.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Submit extends React.Component {
+
+    render() {
+        return (
+            <div>
+            <form action="/api/submit" method="POST" encType="multipart/form-data"> 
+            <p>Please select the file you would like to upload. </p>
+            <input type="file" name="upload"/> <br/> <input type="submit" value="Upload File"/>
+            </form>
+            </div>
+        );
+    }
+}
+
+export default Submit;
