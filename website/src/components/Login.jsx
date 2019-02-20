6d3d1dd157ea9979611d420ad7d415f3e9472d91
Aishwarya Ramanathan <a.ramanathan@mail.utoronto.ca>
2019-02-19 20:50:38 -0500

    Added login endpoint

--- website/src/components/Login.jsx
+++ website/src/components/Login.jsx
@@ -1,35 +1,76 @@
 import React from 'react';
+import $ from 'jquery';
 
 class Login extends React.Component {
     constructor() {
         super();
         this.state = {
             email: "",
-            password: "",
-            username: ""
+            password: ""
         }
     }
 
-    componentWillMount() {
+    componentDidMount() {
         document.addEventListener("keypress", this.handleEnterKeyPress.bind(this));
     }
 
-    addError(type: string) {
+    addLoginError(type: string) {
+        $('.error-message').remove();
         var message = "";
         if (type === 'email1') {
-            message = "Please enter a email";
+            message = "Please enter an email"
         }
         else if (type === 'email2') {
             message = "Please enter a valid email";
         }
-        else if (type === 'password1') {
+        else if (type === 'password') {
             message = "Please enter a password";
         }
         else if (type === 'login') {
-            message = "The username or password is incorrect/invalid";
+            message = "The email or password is incorrect/invalid"
         }
         var errorMessage = '<p class="error-message">' + message + '</p>';
         $('.auth-button').after(errorMessage);
+    }
+
+    validateEmail(email) {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }
+
+    login() {
+        console.log("test");
+        console.log(this.state);
+        if (this.state.email == "") {
+            this.addLoginError('email1');
+            return;
+        }
+        if (!this.validateEmail(this.state.email)) {
+            this.addLoginError('email2');
+            return;
+        }
+        if (this.state.password == "") {
+            this.addLoginError('password');
+            return;
+        }
+        const requestData = JSON.stringify({
+            "email": this.state.username,
+            "password": this.state.password
+        });
+
+        $.ajax({
+            url: 'api/login',
+            type: 'POST',
+            data: requestData,
+            contentType: 'application/json',
+            success: () => {
+                window.location.replace("/home");
+            },
+            error: () => {
+                this.addLoginError('login');
+                return;
+            }
+        });
     }
 
     handleEnterKeyPress(e: any) {
@@ -54,9 +95,9 @@ class Login extends React.Component {
         return (
             <div className="auth-form-container">
                 <form className="login-form" id="login-form">
-                    <input type="text" placeholder="email" onChange={this.handleEmailChange.bind(this)} />
+                    <input type="email" placeholder="email" onChange={this.handleEmailChange.bind(this)} />
                     <input type="password" placeholder="password" onChange={this.handlePasswordChange.bind(this)} />
-                    <div className="auth-button">login</div>
+                    <div className="auth-button" onClick={this.login.bind(this)}>login</div>
                 </form>
             </div>
 
--- website/src/routes.js
+++ website/src/routes.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import AppWrapper from './components/App';
 import Home from './components/Home';
-import About from './components/About';
 import Login from './components/Login';
 import Replay from './components/GameReplay';
 import Submit from './components/Submit';
@@ -13,7 +12,6 @@ class Routes extends React.Component {
             <AppWrapper>
                 <Switch>
                     <Route exact path='/' component={Login} />
-                    <Route path='/about' component={About} />
                     <Route path='/home' component={Home} />
                     <Route path='/replay' component={Replay} />
                     <Route path='/submit' component={Submit} />
