f4ea46f34ecf606dba5493daf0e482e749bf9498
JoLeung <jo.leung@mail.utoronto.ca>
2019-02-19 21:42:42 -0500

    Preemptive docstring for test_client/helper_classes.py, needs review

--- game/test_client/helper_classes.py
+++ game/test_client/helper_classes.py
@@ -48,8 +48,9 @@ class Map: # all outputs will be of the form (x, y). i.e., (c, r).
         return self.grid[y][x].lower() == 'r'
 
     def find_all_resources(self):
-        """(None) -> str
-        # Returns the (x, y) coordinates for all resource nodes.
+        """(None) -> [(int, int)]
+        Returns the (x, y) coordinates for all resource nodes.
+        """
         locations = []
         for row in range(len(self.grid)):
             for col in range(len(self.grid[row])):
@@ -58,7 +59,9 @@ class Map: # all outputs will be of the form (x, y). i.e., (c, r).
         return locations
 
     def closest_resources(self, unit):
-        # Returns the coordinates for the closest node to unit.
+        """(Unit) -> (int, int)
+        Returns the coordinates of the closest resource to <unit>.
+        """
         locations = self.find_all_resources()
         c, r = unit.position()
         result = None
@@ -75,31 +78,43 @@ class Map: # all outputs will be of the form (x, y). i.e., (c, r).
 
 class Units:
     def __init__(self, units):
+        """(___) -> None
+        Initialize a new Units.
+        """
         self.units = {} # a dictionary of unit objects.
         for unit in units:
             self.units[str(unit['id'])] = Unit(unit)
 
     def get_unit(self, id):
+        """(str) -> Unit
+        Return the Unit with <id>.
+        """
         return self.units[id]
 
     def get_all_unit_ids(self):
-        # Returns the id of all current units.
+        """(None) -> [str]
+        Returns the id of all current units.
+        """
         all_units_ids = []
         for id in self.units:
             all_units_ids.append(id)
         return all_units_ids
     
     def get_all_unit_of_type(self, type):
-        # Returns a list of unit objects of a given type.
+        """(str) -> [Unit]
+        Returns a list of unit objects of a given type.
+        """
         all_units = []
         for id in self.units:
             if self.units[id].type == type:
                 all_units.append(self.units[id])
-                    
         return all_units
 
 class Unit:
     def __init__(self, attr):
+        """(___) -> None
+        Initialize a new Unit.
+        """
         self.attr = attr
         self.type = attr['type'] # 'worker' or 'melee'.
         self.x = attr['x']
@@ -107,10 +122,15 @@ class Unit:
         self.id = attr['id']
 
     def position(self):
+        """(None) -> int, int
+        Returns the current position of this Unit.
+        """
         return self.x, self.y
 
     def direction_to(self, pos):
-        # returns the direction from a unit to a certain position (pos[0], pos[1]).
+        """((int, int)) -> Direction
+        Returns the direction from a unit to <pos>.
+        """
         if self.y < pos[1]:
             return 'DOWN'
         elif self.y > pos[1]:
@@ -121,14 +141,22 @@ class Unit:
             return 'RIGHT'
 
     def move(self, *directions):
+        """(___) -> Move
+        Returns a Move for this Unit using the given <*directions>.
+        """
         return Move(self.id, *directions)
  
     def move_towards(self, dest):
+        """((int, int)) -> Move
+        Return a Move for this Unit towards <dest>.
+        """
         direction = self.direction_to(dest)
         return Move(self.id, direction)
 
     def nearby_enemies_by_distance(self, enemy_units):
-        # returns a sorted list of ids and their distances.
+        """(Units) -> [(str, int)]
+        Returns a sorted list of ids and their distances (in a tuple).
+        """
         x = self.x
         y = self.y
         enemies = []
@@ -142,9 +170,15 @@ class Unit:
         return enemies
     
     def attack(self, *directions):
+        """(___) -> Move
+        Return an 'attack' Move for this Unit in the given <*directions>.
+        """
         return Move(self.id, 'ATTACK', *directions)
 
     def can_attack(self, enemy_units): # make this a new function called attack_list and make can_attack a directed function at an enemy unit.
+        """(Units) -> [(Unit, Direction)]
+        Returns a list of enemy Unit that can be attacked and the direction needed to attack them.(?)
+        """
         enemies = []
         for id in enemy_units.units:
             unit = enemy_units.get_unit(id)
@@ -154,32 +188,46 @@ class Unit:
         return enemies
 
     def can_duplicate(self, resources):
+        """(int) -> bool
+        Returns if this Unit can duplicate.
+        """
         if self.type == 'melee' and self.attr['resource_cost'] <= resources and self.attr['duplication_status'] <= 0:
             return True
         else:
             return False
     
     def can_mine(self, game_map):
+        """(Map) -> bool
+        Returns if this Unit can mine.
+        """
         if self.type == 'worker' and game_map.is_resource(self.x, self.y) and self.attr['mining_status'] <= 0:
             return True
         else:
             return False
 
     def mine(self):
+        """(None) -> Move
+        Returns a 'mine' Move for this Unit.
+        """
         return Move(self.id, 'MINE')
 
     def duplicate(self, direction):
+        """(Direction) -> Move
+        Returns a 'duplicate' Move for this Unit in the given <direction>.
+        """
         return Move(self.id, 'DUPLICATE', direction)
 
     def bfs(self, game_map, dest):
-        # Finds the shortest path from current location to dest. Returns a list where the first entry is current position.
+        """(Map, (int, int)) -> [(int, int)]
+        Finds the shortest path from current location to dest. Returns a list where the first entry is current position.
+        """
         graph = game_map.grid
         start = (self.x, self.y)
         queue = [[start]]
         vis = set(start)
         if start == dest:
             return None
-
+        
         while queue:
             path = queue.pop(0)
             node = path[-1]
