98de3c308b6b78eabe2b02f7d3ba1b50539f347d
Kur <kuriki>
2020-11-21 12:06:14 -0500

    Enable registration temporarily

--- website/server/app.py
+++ website/server/app.py
@@ -13,12 +13,13 @@ import docker
 import time
 import shutil
 import os
+import re
 
 '''Main wrapper for app creation'''
 app = Flask(__name__, static_folder='../build')
 app.secret_key = b'a*\xfac\xd4\x940 m\xcf[\x90\x7f*P\xac\xcdk{\x9e3)e\xd7q\xd1n/>\xec\xec\xe0'
 CORS(app)
-database = MongoClient("mongodb+srv://utmrobotics:<password>@deerhunt.ntpnz.mongodb.net/<dbname>?retryWrites=true&w=majority").deerhunt_db
+database = MongoClient("mongodb+srv://utmrobotics:1d3erhunted3089@deerhunt.ntpnz.mongodb.net/<dbname>?retryWrites=true&w=majority").deerhunt_db
 board = Leaderboard(database.leaderboard)
 # dock = docker.from_env()
 
@@ -176,7 +177,7 @@ def changePassword():
 
 @app.route('/api/register', methods=['POST'])
 def register():
-    admin_guard()
+    #admin_guard()
 
     u, p = safe_get_user_and_pass()
 
