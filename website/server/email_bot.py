6ac90bc962b5b0a2cb452a41673134ebdfe05e2d
alex <linalex7@protonmail.com>
2020-12-21 04:22:43 -0500

    Email sending now working

--- website/server/email_bot.py
+++ website/server/email_bot.py
@@ -1,4 +1,6 @@
 import smtplib as smt
+from email.message import EmailMessage
+import ssl
 
 class EmailBot():
     """
@@ -26,13 +28,32 @@ class EmailBot():
         """
         self.sender = sender
         self.password = password
-        self.smtp = smt.SMTP('smtp.gmail.com',587)
-        self.smtp.ehlo()
-        self.smtp.starttls()
-        self.smtp.ehlo()
-        self.smtp.login(self.sender,self.password)
+        # self.context = ssl.create_default_context()
+        # try:
+        #     self.server = smt.SMTP("smtp.gmail.com", 587)
+        #     self.server.ehlo()
+        #     self.server.starttls()
+        #     self.server.login(self.sender, self.password)
+        #     # self.server.sendmail(FROM, TO, message)
+        #     # self.server.close()
+        #     print('successfully sent the mail')
+        # except Exception:
+        #     print("failed to send mail")
+        # # try:
+        # #     server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        # #     server.ehlo()
+        # #     server.login(self.sender,self.password)
+        # try:
+        #     self.smtp = smt.SMTP()
+        #     self.smtp.connect('smtp.gmail.com', '587')
+        #     self.smtp.ehlo()
+        #     self.smtp.starttls(context=self.context)
+        #     self.smtp.login(self.sender,self.password)
+        # except Exception:
+        #     print('something went wrong when making email bot.')
 
-    def sendmail(self,receiver:str, subject:str, body:str) -> bool:
+    @staticmethod
+    def sendmail(receiver:str, subject:str, body:str) -> bool:
         """
         Send a email to the receiver with given subject and body text.
         :param receiver: email id of the receiver.
@@ -41,5 +62,20 @@ class EmailBot():
         :return: returns true if the email was sent successfully and 
                 false otherwise.
         """
+        sender = 'robotics@utmsu.ca'
+        password = 'autonomousenthusiasts'
         message = 'Subject: {}\n\n{}'.format(subject, body)
-        self.smtp.sendmail(self.sender, receiver, message)
+        context = ssl.create_default_context()
+        print(receiver)
+        print(message)
+        with smt.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+            try:
+                # server = smt.SMTP("smtp.gmail.com", 587)
+                # server.starttls()
+                server.login(sender, password)
+                server.ehlo()
+                server.sendmail(sender, receiver, message)
+                # server.close()
+                print('successfully sent the mail')
+            except Exception:
+                print("failed to send mail")
