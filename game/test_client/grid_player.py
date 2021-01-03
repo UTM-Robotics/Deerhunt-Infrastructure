60be6d9ab6ef1c8cd4f022d95e3d62eebec56f9f
Sam <sam.newby@mail.utorontu.ca>
2021-01-03 01:46:27 -0500

    Updates test_client to show attacks and stuns

--- game/test_client/grid_player.py
+++ game/test_client/grid_player.py
@@ -9,29 +9,14 @@ class GridPlayer:
     def __init__(self) -> None:
         self.safe_turns = 0
 
-    def set_safety(self, safe: int) -> None:
-        self.safe_turns = safe
-
-    def pre_game_calc(self, game_map: Map, your_units: Units) -> None:
-        # workers = your_units.get_all_unit_of_type('worker')
-        melees = your_units.get_all_unit_of_type('melee')
-        resource_nodes = game_map.find_all_resources()
-        column = len(game_map.grid[0])
-        row = len(game_map.grid)
-        print("MAP SIZE: {0} x {1}".format(column, row))
-
-        enemy_distance = []
-        self.set_safety((column, row))
-
     def tick(self, game_map: Map, your_units: Units, enemy_units: Units,
              resources: int, turns_left: int) -> [Move]:
 
         print("\n--------RESOURCES: {0} | TURNS LEFT: {1}--------".format(
             resources, turns_left))
 
-        if turns_left == 100:
-            # Pre-game calculations.
-            self.pre_game_calc(game_map, your_units)
+        #if turns_left == 100:
+        #   do pregrame calcs
 
         workers = your_units.get_all_unit_of_type(WORKER_TYPE)
         melees = your_units.get_all_unit_of_type(MELEE_TYPE)
@@ -59,9 +44,12 @@ class GridPlayer:
         for unit in melees:
             enemy_list = unit.nearby_enemies_by_distance(enemy_units)
             if enemy_list:
-                attack_list = unit.can_stun(enemy_units)
+                attack_list = unit.can_attack(enemy_units)
+                stun_list = unit.can_stun(enemy_units)
                 if attack_list:
-                    moves.append(unit.stun(attack_list[0][1]))
+                    moves.append(unit.attack(attack_list[0][1]))
+                elif stun_list:
+                    moves.append(unit.stun(stun_list[0][1]))
                 else:
                     closest = enemy_units.units[enemy_list[0][0]]
                     moves.append(unit.move_towards((closest.x, closest.y)))
