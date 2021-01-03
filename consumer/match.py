9cc00edf41842b5ee56fef101b1f061a98a63e8c
Sam <sam.newby@mail.utorontu.ca>
2021-01-02 23:21:47 -0500

    Adds update leaderboard to consumer

--- consumer/match.py
+++
@@ -1,9 +0,0 @@
-class Match:
-    def __init__(self, challenger, defender):
-        self.result = ""
-        self.challenger = challenger
-        self.defender = defender
-
-    #runMatch runs the match and returns the result of the match
-    def runMatch(self):
-        pass
