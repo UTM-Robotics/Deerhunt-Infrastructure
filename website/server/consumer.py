814f840b3f1f10eb69d1b5011af61ff07f464520
Sam <sam.newby@mail.utorontu.ca>
2021-01-07 18:27:17 -0500

    Fixes consumer crash bug when file not found

--- website/server/consumer.py
+++ website/server/consumer.py
@@ -3,6 +3,7 @@ from teams import TeamController
 from game_runner import GameController
 from storage import StorageAPI
 from datetime import datetime
+from time import sleep
 DATABASE_URL = "mongodb+srv://utmrobotics:1d3erhunted3089@deerhunt.ntpnz.mongodb.net/<dbname>?retryWrites=true&w=majority"
 PROD_FLAG = True
 
@@ -26,24 +27,33 @@ class Consumer:
             #Get the next match and run the match if there is one (by order of modified)
             match = self.submission_queue.find_one(sort=[("modified", 1)])
             if match is not None:
-                self.submission_queue.delete_one({'_id': match['_id']})
-                #Runs the match and gets the result
-                match_string = self.create_match(match)
-                if match_string is not None and not isinstance(match_string, int):
-                    result = GameController.run_game(self.create_match(match))
-                    self.database.logs.insert_one({"winner": result[1], "data": result[0], "team_id": self.challenger['_id'], "modified": datetime.now()})
-                    self.update_leaderboard(result)
-                elif match_string is None:
-                    return
-                elif match_string == StorageAPI.P1_ZIP_ERROR: # Handle invalid zip file.
-                    match_result = {'lines': [],
-                        'maps': [],
-                        'errors': [],
-                        'build_id': []
-                        }
-                    result = (match_result,1)
-                    self.update_leaderboard(result)
-                #Removes the match from the submission queue
+                print("Got match")
+                try:
+                    self.submission_queue.delete_one({'_id': match['_id']})
+                    #Runs the match and gets the result
+                    match_string = self.create_match(match)
+                    if match_string is not None and not isinstance(match_string, int):
+                        print("got match {}".format(match_string))
+                        result = GameController.run_game(match_string)
+                        self.database.logs.insert_one({"winner": result[1], "data": result[0], "team_id": self.challenger['_id'], "modified": datetime.now()})
+                        self.update_leaderboard(result)
+                    elif match_string is None:
+                        return
+                    elif match_string == StorageAPI.P1_ZIP_ERROR: # Handle invalid zip file.
+                        print("Got bad match")
+                        match_result = {'lines': [],
+                            'maps': [],
+                            'errors': [],
+                            'build_id': []
+                            }
+                        result = (match_result,1)
+                        self.update_leaderboard(result)
+                    #Removes the match from the submission queue
+                except FileNotFoundError:
+                    print("File not found {}".format(self.challenger['_id']))
+            else:
+                sleep(30)
+                
     
     def create_match(self, match: dict) -> str:
         '''create_match creates a new match given the team id's and returns the result '''
@@ -67,12 +77,15 @@ class Consumer:
         self.session.start_transaction()
         current_leaderboard = self.database.leaderboard.find_one({'type': 'current'})
         defending_rank, attacking_rank = -1, -1
+        print("Updating leaderboard")
         #Gets the defending and challenging team from the leaderboard
         for k,v in enumerate(current_leaderboard["teams"]):
             if v == self.defender['name']:
                 defending_rank = k
             if v == self.challenger['name']:
                 attacking_rank = k
+
+        print("Got ranks {} and {}".format(defending_rank, attacking_rank))
 
         #Adds the defender to the bottom of the leaderboard if it is their first submission
         if defending_rank == -1:
@@ -83,6 +96,7 @@ class Consumer:
 
         #If the attacker wins insert them into the defenders spot
         if result[0]['lines'][-1] == 'Winner: p1':
+            print("Player 1 won")
             new_leaderboard = current_leaderboard["teams"]
             if attacking_rank != -1: 
                 new_leaderboard.remove(self.challenger['name'])
@@ -93,6 +107,7 @@ class Consumer:
 
         #Adds attacker to bottom if it is its first submission to the leaderboard
         if attacking_rank == -1:
+            print("Attacker not on board and lost add to end")
             new_leaderboard = current_leaderboard["teams"]
             new_leaderboard.append(self.challenger['name'])
             self.database.leaderboard.update_one({"type": "current"}, {"$set" : {"teams": new_leaderboard}}) 
