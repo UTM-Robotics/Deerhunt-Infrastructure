f6a963d0efddd4f67ba7672b8e6869089af7d27d
alex <linalex7@protonmail.com>
2021-06-06 16:15:14 -0400

    Started development of testing. Using diff file based testing. Built base class which all test functions will use.

--- server/app.py
+++ server/app.py
@@ -1,13 +1,19 @@
 from .config import Configuration
-
-from flask import Flask
-from flask_restful import Api
+from http import HTTPStatus
+from flask import Flask, make_response
+from flask_restful import Api, Resource
 from flask_cors import CORS
 
 
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
+
+class Auth(Resource):
+	def post(self):
+		return make_response("Post workss\n", HTTPStatus.OK)
+
+api.add_resource(Auth, '/register')
 
 
 if __name__ == '__main__':
