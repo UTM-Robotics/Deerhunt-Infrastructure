b6fa0e0a1c133c1d369f3e57cd892fcefcfb6f8f
alex <linalex7@protonmail.com>
2021-06-06 20:30:49 -0400

    Seperated Register route into subdirectory.

--- server/app.py
+++ server/app.py
@@ -1,19 +1,20 @@
-from .config import Configuration
-from http import HTTPStatus
-from flask import Flask, make_response
-from flask_restful import Api, Resource
+from flask import Flask
+from flask_restful import Api
 from flask_cors import CORS
 
+from pymongo import MongoClient
+
+from .config import Configuration
+from .Auth.register import RegisterRoute
 
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
 
-class Auth(Resource):
-	def post(self):
-		return make_response("Post works\n", HTTPStatus.OK)
+# Mongo = MongoClient(Configuration.MONGODB_URI)
 
-api.add_resource(Auth, '/register')
+
+api.add_resource(RegisterRoute, '/register')
 
 
 if __name__ == '__main__':
