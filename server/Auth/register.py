6228666ea5644a7b913132c47fecdc63c529d143
alex <linalex7@protonmail.com>
2021-06-13 17:51:22 -0400

    enhanced email sending features.

--- server/Auth/register.py
+++ server/Auth/register.py
@@ -1,13 +1,14 @@
 from flask_restful import Api, Resource
 from flask import make_response, request
 from http import HTTPStatus
-from .emailbot import EmailBot
+from ..EmailBot.emailbot import EmailBot
 
 
 class RegisterRoute(Resource):
     def post(self):
-        print(request.values)
-        # result = database.users.find_one({'username': u})
-        # with EmailBot() as email:
-            # email.send_register()
+        print(request.json)
+        username = request.json['username']
+        # result = database.users.find_one({'username': u})  <--- Need DB setup
+        with EmailBot(purpose='registration') as email:
+            email.send(username)
         return make_response("Post works\n", HTTPStatus.OK)
---
+++ server/EmailBot/emailbot.py
@@ -0,0 +1,37 @@
+import smtplib, ssl
+from email.message import EmailMessage
+import os
+from ..config import Configuration
+
+Email_Titles = {'registration': 'Welcome to Deerhunt!'}
+
+
+class EmailBot:
+    def __init__(self, purpose=None):
+        self.purpose = purpose
+        self.port = 465
+
+    def __enter__(self):
+        self.sender = Configuration.FROM_EMAIL_ADDR
+        self.password = Configuration.FROM_EMAIL_PASS
+        self.context = ssl.create_default_context()
+        self.build_message()
+        return self
+
+    def __exit__(self, type, value, tb):
+        pass
+
+    def build_message(self):
+        print(os.getcwd())
+        with open('EmailBot/{}.html'.format(self.purpose)) as file:
+            self.msg = EmailMessage()
+            self.msg.set_content(file.read(), subtype='html')
+        self.msg['Subject'] = Email_Titles[self.purpose]
+        self.msg['From'] = self.sender
+
+    def send(self, receiver):
+        with smtplib.SMTP_SSL("smtp.gmail.com", self.port, context=self.context) as email:
+            email.login(self.sender, self.password)
+            # email.sendmail(self.sender, '{}@mail.utoronto.ca'.format(receiver), self.message)
+            self.msg['To'] = '{}@mail.utoronto.ca'.format(receiver)
+            email.send_message(self.msg)
