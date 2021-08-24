afff248d6429bfa62a1da5082ec73bba9650c8dc
Alex <linalex7@protonmail.com>
2021-08-24 16:26:16 -0400

    Added template code for teams. Also reorganizing some files into folders. Tests passing

---
+++ server/Routes/Teams/Teams.py
@@ -0,0 +1,18 @@
+from http import HTTPStatus
+from flask import make_response, request, abort, jsonify
+from flask_restful import Resource
+
+from server.Managers.Teams.TeamsManager import TeamsManager
+
+from server.Managers.Auth.UserManager import auth
+
+
+class TeamsRoute(Resource):
+
+    @auth.login_required
+    def post(self):
+        email = auth.current_user()
+        with UserManager(auth.current_user()) as usermanager:
+            pass # check that user isn't part of team
+        with TeamsManager(request.json['name']) as teamsmanager:
+            pass # check that new team 'name' doesn't exist already. Creates if doesn't.
