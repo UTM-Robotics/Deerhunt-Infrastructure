9bb7ed321329baa2039c20662c0a5b772cb7d68f
AlexLin <linalex7@protonmail.com>
2021-11-09 15:48:04 +0800

    added form login form submission function. Flask receives data now

--- deerhunt/src/components/LoginForm.js
+++ deerhunt/src/components/LoginForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import {
   Flex,
   Box,
@@ -8,8 +9,26 @@ import {
   Input,
   Button,
 } from "@chakra-ui/react";
+import axios from "axios"
 
-const LoginForm = () => {
+export default function LoginForm() {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting }
+  } = useForm();
+
+  async function login(values) {
+    var form = new FormData()
+    form.append("email", values.email)
+    form.append("password", values.password)
+    await axios.post("http://127.0.0.1:5000/api/login",form).then((response) => {
+      console.log(response)
+    }).catch(() => {
+      console.log("failed to register")
+    })
+  }
+
   return (
     <Flex
       minHeight="100vh"
@@ -32,17 +51,26 @@ const LoginForm = () => {
             <Heading>Login to Deerhunt</Heading>
           </Box>
           <Box>
-            <form>
+            <form onSubmit={handleSubmit(login)}>
               <FormControl>
                 <FormLabel>Email</FormLabel>
-                <Input type="email" placeholder="Enter Your Email" />
+                <Input type="email" placeholder="Enter Your Email" 
+                {...register("email", {
+                  required: "This is required",
+                })}
+                />
               </FormControl>
               <FormControl mt={4}>
                 <FormLabel>Password</FormLabel>
-                <Input type="password" placeholder="Enter Your Password" />
+                <Input type="password" placeholder="Enter Your Password" 
+                {...register("password", {
+                  required: "This is required",
+                  minLength: { value: 8, message: "Minimum length should be 4" }
+                })}
+                />
               </FormControl>
               <Box my={4}>
-                <Button width="full">Login</Button>
+              <Button width="full" isLoading={isSubmitting} type="submit">Login</Button>
               </Box>
               <Box my={4}>
                 <Button width="full">Create An Account</Button>
@@ -55,4 +83,50 @@ const LoginForm = () => {
   );
 };
 
-export default LoginForm;
+// const LoginForm = () => {
+//   return (
+//     <Flex
+//       minHeight="100vh"
+//       width="full"
+//       justifyContent="center"
+//       alignItems="center"
+//     >
+//       <Box>
+//         <Box
+//           borderWidth={1}
+//           px={6}
+//           py={6}
+//           borderRadius={4}
+//           boxShadow="lg"
+//           width="full"
+//           maxWidth="500px"
+//           bg="gray.300"
+//         >
+//           <Box textAlign="center" mb={4}>
+//             <Heading>Login to Deerhunt</Heading>
+//           </Box>
+//           <Box>
+//             <form>
+//               <FormControl>
+//                 <FormLabel>Email</FormLabel>
+//                 <Input type="email" placeholder="Enter Your Email" />
+//               </FormControl>
+//               <FormControl mt={4}>
+//                 <FormLabel>Password</FormLabel>
+//                 <Input type="password" placeholder="Enter Your Password" />
+//               </FormControl>
+//               <Box my={4}>
+//                 <Button width="full">Login</Button>
+//               </Box>
+//               <Box my={4}>
+//                 <Button width="full">Create An Account</Button>
+//               </Box>
+//             </form>
+//           </Box>
+//         </Box>
+//       </Box>
+//     </Flex>
+//   );
+// };
+
+// export default LoginForm;
