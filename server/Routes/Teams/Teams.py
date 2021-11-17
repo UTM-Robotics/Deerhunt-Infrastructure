94e283f2ef5e947018b27859ccd968b9ee981335
Alex Lin <linalex7@protonmail.com>
2021-11-17 21:40:59 +0800

    fixing teams with parser

--- server/Routes/Teams/Teams.py
+++ server/Routes/Teams/Teams.py
@@ -1,7 +1,7 @@
 from http import HTTPStatus
-import json
+
 from flask import make_response, request, abort, jsonify
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 
 from server.Managers.Teams.TeamsManager import TeamsManager
 from server.Managers.Auth.UserManager import UserManager
@@ -11,12 +11,17 @@ from server.Managers.Auth.UserManager import User_auth
 
 class TeamsRoute(Resource):
 
+    # flask parser for post request
+    parser = reqparse.RequestParser()
+    parser.add_argument('name', type=str, required=True, help='This field cannot be left blank')
+
     @User_auth.login_required
     def post(self):
         email = User_auth.current_user()
+        data = TeamsRoute.parser.parse_args()
         # with UserManager(email) as usermanager:
         #     pass # check that user isn't part of team
-        with TeamsManager(request.json['name']) as teamsmanager:
+        with TeamsManager(data['name']) as teamsmanager:
             result = teamsmanager.create_team(email)
             if result:
                 return make_response(jsonify({'message': 'New Team Created'}))
