ce2b4aaa73795b9aec40cdb658f4cd1f5ccea7d6
Alex <linalex7@protonmail.com>
2021-12-23 18:39:30 -0500

    fixed testing to use gmail again rather than 10minutemail which is completely broken. Tests all pass.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,22 @@
 import os
 import pytest
 from dotenv import load_dotenv
-from minutemail import Mail
 
 load_dotenv()
 
-# Create a new 10 minute mail session
-# to imitate user's email account.
-mail = Mail()
+'''
+Pytest default function to set default fixtures.
+Enables command line options for fixtures.
 
-
-# Pytest default function to set default fixtures.
-# Enables command line options for fixtures.
+You can pass in parameters using the .env file or just using command line parameters listed below.
+eg:
+$ pytest --flaskaddr 192.168.0.231:8000
+$ pytest --recvemail testing@gmail.com --passwd abcd1234 --adminusername admin --adminpasswd admin
+'''
 def pytest_addoption(parser):
     parser.addoption('--flaskaddr', action='store', default=os.getenv('FLASK_ADDR'), help='IP address and port of flask server. Default is 127.0.0.1:5000')
+    parser.addoption('--recvemail', action='store', default=os.getenv('TEST_EMAIL_ADDR'), help='Email address to receive test emails from.')
+    parser.addoption('--passwd', action='store', default=os.getenv('TEST_EMAIL_PASSWD'), help='Password for email.')
     parser.addoption('--adminusername', action='store', default=os.getenv('ADMIN_USERNAME'), help='Default admin account username')
     parser.addoption('--adminpasswd', action='store', default=os.getenv('ADMIN_PASSWORD'), help='Default admin account password')
 
@@ -28,7 +31,9 @@ def flaskaddr(request):
 # Used by some test functions to register, read email etc.
 @pytest.fixture
 def receive_email(request):
-    return mail
+    password = request.config.getoption('--passwd')
+    email = request.config.getoption('--recvemail')
+    return f'{email}:{password}'
 
 
 # Pytest fixture for default admin creds.
