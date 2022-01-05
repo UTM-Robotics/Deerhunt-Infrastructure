c1da58fa867179e630ade963dd875976d4bfb3fe
Alex <linalex7@protonmail.com>
2022-01-05 17:54:07 -0500

    fixed consumer api. Tested. Works

--- server/Routes/Consumer/Consumer.py
+++ server/Routes/Consumer/Consumer.py
@@ -1,8 +1,11 @@
 from http import HTTPStatus
 from flask import make_response, request, abort, jsonify
+from bson.json_util import dumps
 from flask_restful import Resource, reqparse
 
 from server.Managers.Matches.MatchRequestManager import MatchRequestManager
+from server.Managers.Events.AdminEvents import EventsManager
+from server.Managers.Auth.UserManager import User_auth
 from server.config import Configuration
 
 
@@ -15,17 +18,21 @@ class ConsumerRoute(Resource):
         if data['token'] != Configuration.CONSUMER_TOKEN:
             return abort(HTTPStatus.UNAUTHORIZED)
 
+    @User_auth.login_required
     def post(self):
-        self.check_auth()
         parser = reqparse.RequestParser()
-        parser.add_argument('teams', type=str, required=True, action="append")
-        parser.add_argument('event_id', type=str, required=True)
+        parser.add_argument('team1_id', type=str, required=True)
+        parser.add_argument('team2_id', type=str, required=True)
+        parser.add_argument('name', type=str, required=True)
         data = parser.parse_args()
-        with MatchRequestManager() as requestmanager:
-            if requestmanager.create_request(data):
-                return make_response(jsonify({'message': 'Successfully created a match request'}), HTTPStatus.OK)
-            else:
-                raise SystemError("Error occurs when create request")
+        with EventsManager(data['name']) as eventmanager:
+            eventdata = eventmanager.find_event()
+            data['event_id'] = eventdata['_id']
+            with MatchRequestManager() as requestmanager:
+                if requestmanager.create_request(data):
+                    return make_response(jsonify({'message': 'Successfully created a match request'}), HTTPStatus.OK)
+                else:
+                    raise SystemError("Error occurs when create request")
 
     # Note: passing token, event_id parameters to route
     def get(self):
@@ -34,7 +41,10 @@ class ConsumerRoute(Resource):
         parser.add_argument('event_id', type=str, required=True)
         data = parser.parse_args()
         with MatchRequestManager() as requestmanager:
-            return requestmanager.find_first_request(data['event_id'])
+            result = requestmanager.find_first_request(data['event_id'])
+            if result:
+                print(result)
+                return make_response(dumps([result]), HTTPStatus.OK)
 
     def delete(self):
         self.check_auth()
