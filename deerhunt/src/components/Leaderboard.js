2e0f0b9b5e2341459c7dded541a66ea1878851ab
Kimberly <kimberly.cao13@gmail.com>
2022-01-05 18:06:36 -0500

    Challenges should work

--- deerhunt/src/components/Leaderboard.js
+++ deerhunt/src/components/Leaderboard.js
@@ -8,22 +8,53 @@ import {
   Td,
   TableCaption,
   Button,
+  AlertIcon,
+  Alert,
+  AlertDescription,
+  CloseButton,
 } from "@chakra-ui/react";
 import axios from "../config/config.js";
 
 const Leaderboard = (props) => {
   const [leaderboard, setLeaderboard] = useState([]);
+  const [myTeam, setMyTeam] = useState("");
 
   useEffect(() => {
+    axios
+      .get("/api/teams", { params: { game: props.event } })
+      .then((response) => {
+        setMyTeam(response.data);
+      });
     axios
       .get("/api/leaderboard", {
         params: { name: props.event },
       })
       .then((response) => {
         setLeaderboard(response.data);
-        console.log(response.data);
       });
   }, []);
+
+  const getChallengeFunction = (team, opponent) => {
+    return () => {
+      var form = new FormData();
+      form.append("name", props.event);
+      form.append("team1_id", team._id);
+      form.append("team2_id", opponent._id);
+      axios
+        .post("/api/requests", form)
+        .then((response) => console.log(response));
+    };
+  };
+
+  const InvalidChallenge = () => {
+    return (
+      <Alert>
+        <AlertIcon />
+        <AlertDescription>Cannot challenge your own team!</AlertDescription>
+        <CloseButton position="absolute" right={4} top={4} />
+      </Alert>
+    );
+  };
 
   return (
     <Table>
@@ -43,7 +74,17 @@ const Leaderboard = (props) => {
             <Td>{index + 1}</Td>
             <Td>{team.name}</Td>
             <Td>
-              <Button>Challenge</Button>
+              <Button
+                onClick={
+                  myTeam._id != team._id ? (
+                    getChallengeFunction(myTeam, team)
+                  ) : (
+                    <InvalidChallenge />
+                  )
+                }
+              >
+                Challenge
+              </Button>
             </Td>
           </Tr>
         ))}
