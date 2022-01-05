e13820c150643017afba367c572c2db3f9ec2bd2
Kimberly <kimberly.cao13@gmail.com>
2022-01-05 03:43:19 -0500

    Display team information

--- deerhunt/src/components/TeamsTable.js
+++ deerhunt/src/components/TeamsTable.js
@@ -1,43 +1,49 @@
 import React, { useState, useEffect } from "react";
-import { Heading, Stack, Text, IconButton, Tooltip } from "@chakra-ui/react";
+import {
+  Heading,
+  Stack,
+  Text,
+  IconButton,
+  Tooltip,
+  UnorderedList,
+  ListItem,
+  Center,
+} from "@chakra-ui/react";
 import { FaEdit } from "react-icons/fa";
 import axios from "../config/config.js";
 
-const mockTeamsData = [
-  {
-    team: "Team A",
-    owner: "Joe@mail.utoronto.ca",
-    members: [
-      "Kat@mail.utoronto.ca",
-      "Diane@mail.utoronto.ca",
-      "Brandon@mail.utoronto.ca",
-    ],
-  },
-];
-
 const TeamsTable = () => {
   const [teamsData, setTeamsData] = useState([]);
+  const [membersList, setMembersList] = useState([]);
 
   useEffect(() => {
     axios.get("/api/teams").then((response) => {
-      setTeamsData(response.data);
-      console.log(response.data);
+      setTeamsData(response.data[0]);
+      setMembersList(response.data[0].members);
     });
   }, []);
 
   return (
-    <Stack m={4}>
-      <Heading>
-        Current Team:{" "}
-        <Tooltip label="Edit team">
-          <IconButton icon={<FaEdit />} />
-        </Tooltip>
-      </Heading>
-      <Heading size={"lg"}>Owner</Heading>
-      <Text>Test</Text>
-      <Heading size={"lg"}>Members</Heading>
-      <Text>Test</Text>
-    </Stack>
+    <Center>
+      <Stack m={4}>
+        <Heading>
+          Current Team{" "}
+          <Tooltip label="Edit team">
+            <IconButton icon={<FaEdit />} />
+          </Tooltip>
+        </Heading>
+        <Heading size={"lg"}>Name</Heading>
+        <Text>{teamsData.name}</Text>
+        <Heading size={"lg"}>Owner</Heading>
+        <Text>{teamsData.owner}</Text>
+        <Heading size={"lg"}>Members</Heading>
+        <UnorderedList>
+          {membersList.map((member) => (
+            <ListItem>{member}</ListItem>
+          ))}
+        </UnorderedList>
+      </Stack>
+    </Center>
   );
 };
 
